7f2309449f4df592ca4709563856063eedeebc0e
yuanMXl <48933545+yuanMXl@users.noreply.github.com>
2019-04-29 17:42:31 +0800

    Add files via upload

---
+++ Controllers/QRController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace demo.Controllers
+{
+    public class QrController : Controller
+    {
+        // GET: QR
+        public ActionResult index()
+        {
+            erweima.Generate("http://192.168.124.19/QR/uploadimg");
+            return View();
+        }
+        //[HttpPost]
+        public ActionResult uploadimg()
+        {
+            if (Request.Files.Count > 0)
+            {
+                HttpPostedFileBase f = Request.Files["file1"];
+                f.SaveAs(@"E:\ymx\img" + f.FileName);
+            }
+            return View();
+        }
+
+          
+    }
+}
